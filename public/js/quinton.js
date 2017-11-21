8a3e4525e75bea22378dd338aa3925d23e1e940b
work <work@137.21.150.238>
2017-11-21 14:52:54 -0500

    quinton complete

--- public/js/quinton.js
+++ public/js/quinton.js
@@ -1,74 +1,71 @@
-/*
- * jQuery Conta Code (jquery.contra.js)
- * Konami Contra code implemented in jQuery
- * (http://en.wikipedia.org/wiki/Konami_Code)
- *
- * @author James Brooks <jbrooksuk@me.com>
- * @website http://james.brooks.so
- * @license MIT - http://jbrooksuk.mit-license.org
+/**
+ * this function is a hidden easter egg when you do a certain code on the browser
  */
-
 (function ($) {
-    $.contra = function (config) {
-        var currStep = 0, pressKey, timerStarted = false, keyTimer;
+    if (!$) {
+        /**
+         * pass in jquery object to know if it was loaded. 
+         * Uses jquery for fade function.
+         */
+        return;
+    }
+    /**
+     * keeps track of the current step, pressed key, timer, and the code needed
+     * to be entered by the user
+     */
+    var currStep = 0;
+    var pressKey;
+    var timerStarted = false;
+    var keyTimer;
+    /*Code is left, right, up, down, enter*/
+    var code = [38, 40, 37, 39, 13];
+    /*time in ms between each keypress, 3 seconds*/
+    var time = 3000;
+    /*anywhere on the body of the browser window will trigger this event*/
+    document.body.onkeyup = function (e) {
+        /*grab the key event*/
+        pressKey = e.which || e.keyCode;
 
-        config = config || {};
-        config.begin = config.begin || false;
-        config.success = config.success || false;
-        config.scope = config.scope || $(document);
-        config.code = config.code || [38, 40, 37, 39, 13]; // Up, Down, Left, Right, Enter
-        config.time = config.time || 3000; // Time in ms or false
-        config.fail = config.fail || false;
-
-        config.scope.keyup(function (e) {
-            pressKey = e.which || e.keyCode;
-
-            if (pressKey === config.code[currStep]) {
-                if (!timerStarted && config.time !== false) {
-                    if (typeof config.begin === "function")
-                        config.begin();
-                    keyTimer = setTimeout(function (time) {
-                        timerStarted = false;
-                        clearTimeout(keyTimer);
-                        currStep = 0;
-                    }, config.time);
-                }
-
-                currStep++;
-                timerStarted = true;
-
-                if (currStep >= config.code.length) {
-                    console.log("Success");
-                    if (typeof config.success === "function")
-                        config.success();
+        if (pressKey === code[currStep]) {
+            /*if the current key in the code sequence is pressed, execute*/
+            if (!timerStarted && time !== false) {
+                /*if this is he first key in the sequence, start the timmer*/
+                keyTimer = setTimeout(function () {
+                    /*when the timer has reached its end, clear the timer and
+                     * set the step back to 0*/
                     timerStarted = false;
                     clearTimeout(keyTimer);
                     currStep = 0;
-                }
-            } else {
-                // Call the timeout function
-                if (typeof config.fail === "function")
-                    config.fail();
-                currStep = 0;
+                }, time);
+            }
+            /**
+             * increment the step
+             */
+            currStep++;
+            timerStarted = true;
+
+            if (currStep >= code.length) {
+                /**
+                 * if you reach the end of the sequence do the celebratory dance
+                 */
+                $('body').fadeOut(2000, function () {
+                    /*fade out the all content, replace it with tranquility 
+                     * and fade it back in*/
+                    $('body').html('<h1 class="tranquility">Tranquility<br><em style="font-size:2em;">☺<em></h1>');
+                    $('body').fadeIn(2000);
+                });
+                /*clear the timmer and get rid of the evendence*/
                 timerStarted = false;
                 clearTimeout(keyTimer);
+                currStep = 0;
+                document.body.onkeyup = function () {}
             }
-        });
+        } else {
+            /*if it is the incorrect key, restart the timer*/
+            currStep = 0;
+            timerStarted = false;
+            clearTimeout(keyTimer);
+        }
     };
 
-    $.fn.contra = function (config) {
-        var config = config || {};
-        config.scope = $(this);
-
-        $.contra(config);
-    };
 })(jQuery);
-
-$.contra({
-    success: function() {
-        $('body').fadeOut(2000,function(){
-            $('body').html('<h1 class="tranquility">Tranquility<br><em style="font-size:2em;">☺<em></h1>');
-            $('body').fadeIn(2000);
-        });
-    }
-});
--- views/_shared/_layout.php
+++ views/_shared/_layout.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--dynamic page descriptions and keywords-->
         <meta name="description" content="<?= empty($viewbag['page_description']) ? '' : $viewbag['page_description'] ?>">
         <meta name="keywords" content="<?= empty($viewbag['page_keywords']) ? '' : $viewbag['page_keywords'] ?>">
         <meta name="author" content="Quinton Ward">
@@ -12,11 +13,15 @@
 
         <link href="<?= __relpath__ ?>/public/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?= __relpath__ ?>/public/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <!--individual css for each student-->
         <link href="<?= __relpath__ ?>/public/css/connor.css" rel="stylesheet" type="text/css"/>
         <link href="<?= __relpath__ ?>/public/css/evan.css" rel="stylesheet" type="text/css"/>
         <link href="<?= __relpath__ ?>/public/css/quinton.css" rel="stylesheet" type="text/css"/>
         <link href="<?= __relpath__ ?>/public/css/sam.css" rel="stylesheet" type="text/css"/>
         <?php
+        /**
+         * this allows use to dynamically include css files dependant on our needs.
+         */
         if (!empty($viewbag['page_css']) && is_array($viewbag['page_css'])) {
             foreach ($viewbag['page_css'] as $css) {
                 echo "<link href='" . __relpath__ . "$css' rel='stylesheet' type='text/css'>";
@@ -25,6 +30,7 @@
         ?>
     </head>
     <body>
+        <!--handles all of the navigation, uses bootstrap classes-->
         <nav class="navbar navbar-default" role="navigation">
             <div class="container">
                 <div class="navbar-header">
@@ -34,19 +40,57 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?= __relpath__ ?>/">Home</a>
+                    <a class="navbar-brand" href="<?= __relpath__ ?>/">Dynauth.io</a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
+                        <li><a href="<?= __relpath__ ?>/">Home</a></li>
                         <li><a href="<?= __relpath__ ?>/test">Test</a></li>
                         <li><a href="<?= __relpath__ ?>/about">About Us</a></li>
                         <li><a href="<?= __relpath__ ?>/dashboard">Dashboard</a></li>
+                        <li><a href="http://blog.cis442.dynauth.io/">Blog</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
+        <div class="body-wrapper-class">
+            <?php 
+            /**
+             * includes the meat of the file
+             */
+            require __ecom__ . "$viewbag[page_path]"; 
+            ?>
+        </div>
 
-        <?php require __ecom__ . "$viewbag[page_path]"; ?>
+        <footer >
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h6>Address</h6>
+                        <p>
+                            The Frontier<br>
+                            800 Park Offices Drive<br>
+                            Research Triangle Park, NC
+                        </p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h6>Hours</h6>
+                        <p>
+                            Monday—Friday: 9:00AM–5:00PM<br>
+                            Saturday &amp; Sunday: 11:00AM–3:00PM</p>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h6>About This Site</h6>
+                        <p>Our mission is to secure the infrastructure of the Internet to create a safe and productive environment accessible for everyone everywhere.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
         <script src="<?= __relpath__ ?>/public/vendors/jquery/jquery.min.js" type="text/javascript"></script>
         <script src="<?= __relpath__ ?>/public/vendors/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="<?= __relpath__ ?>/public/js/connor.js" type="text/javascript"></script>
@@ -56,6 +100,9 @@
         <script src="<?= __relpath__ ?>/public/js/quinton.js" type="text/javascript"></script>
         <script src="<?= __relpath__ ?>/public/js/sam.js" type="text/javascript"></script>
         <?php
+        /**
+         * again, dynamically insert javascript files
+         */
         if (!empty($viewbag) && !empty($viewbag['page_scripts']) && is_array($viewbag['page_scripts'])) {
             foreach ($viewbag['page_scripts'] as $script) {
                 echo "<script src='" . __relpath__ . "$script'></script>";
